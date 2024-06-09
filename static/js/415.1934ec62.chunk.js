344a289c565f8228ab211a1f88cc01da1312d097
gointoorbit <130983897+gointoorbit@users.noreply.github.com>
2024-06-09 20:26:15 +0000

    Deploying to gh-pages from @ gointoorbit/goit-react-hw-08-phonebook@11d2136dff3326131aa8a2a07030be22da744868 🚀

---
+++ static/js/415.1934ec62.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(o,e,n){n.r(e);var h=n(184);e.default=function(){return(0,h.jsxs)("main",{children:[(0,h.jsx)("h1",{children:"Phonebook"}),(0,h.jsx)("h3",{children:"Welcome! This is your phonebook application."})]})}}}]);
+//# sourceMappingURL=415.1934ec62.chunk.js.map
